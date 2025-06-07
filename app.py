faf32cde4601df3af9b0a2725c0d5323b64ff7fd
bokuldutta09 <154738052+bokuldutta09@users.noreply.github.com>
2025-06-07 18:00:10 +0530

    app.py

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ import pandas as pd
 import requests
 
 
-# try:
-#     response = requests.get("https://api.themoviedb.org/3/movie/10764?api_key=1df1226cb641a0772921bc2a41d7c2c2&language=en-US", timeout=50)
-#     print(response.status_code)
-#     print(response.json())
-# except Exception as e:
-#     print(f"Error: {e}")
-
 
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=1df1226cb641a0772921bc2a41d7c2c2&language=en-US"
@@ -54,11 +47,8 @@ selected_movie_name = st.selectbox("Select movie to get recommendations", movies
 
 if st.button("Show Recommendation"):
     recommended_movies, recommended_movies_poster = recommend(selected_movie_name)
-    # col1, col2, col3, col4, col5 = st.columns(5)
-    # print("Posters URLs:", recommended_movies_poster)
 
     cols = st.columns(5)
-    # Fallback image url (you can replace with your own or a local file)
     fallback_image_url = "https://via.placeholder.com/300x450?text=No+Image"
 
     for i in range(5):
@@ -66,7 +56,6 @@ if st.button("Show Recommendation"):
             st.image(recommended_movies_poster[i]
                      if recommended_movies_poster[i] else fallback_image_url, use_container_width=True)
 
-            # Show the title in a fixed height box to keep alignment
             st.markdown(
                 f"""
                             <div style='text-align: center; height: 3em; overflow: hidden; font-weight: bold;'>
@@ -79,36 +68,3 @@ else:
     st.write("No image to display")
 
 
-
-#     with col1:
-#         st.text(recommended_movies[0])
-#         if recommended_movies_poster[0]:
-#             st.image(recommended_movies_poster[0], use_container_width=True)
-#         else:
-#             st.image(fallback_image_url)
-#     with col2:
-#         st.text(recommended_movies[1])
-#         if recommended_movies_poster[1]:
-#             st.image(recommended_movies_poster[1], use_container_width=True)
-#         else:
-#             st.image(fallback_image_url)
-#     with col3:
-#         st.text(recommended_movies[2])
-#         if recommended_movies_poster[2]:
-#             st.image(recommended_movies_poster[2], use_container_width=True)
-#         else:
-#             st.image(fallback_image_url)
-#     with col4:
-#         st.text(recommended_movies[3])
-#         if recommended_movies_poster[3]:
-#             st.image(recommended_movies_poster[3], use_container_width=True)
-#         else:
-#             st.image(fallback_image_url)
-#     with col5:
-#         st.text(recommended_movies[4])
-#         if recommended_movies_poster[4]:
-#             st.image(recommended_movies_poster[4], use_container_width=True)
-#         else:
-#             st.image(fallback_image_url)
-# else:
-#     st.write("No image to display")
